30bcffb923aed0abbfdeabf376191d5b42e8ad62
Prelang Builder <builder@prelang.com>
2014-03-24 15:06:33 +0000

    Initial commit; clean Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wahrnit::Application.config.secret_key_base = '263a6d9f0536207e61252d64c46e9ff0b87e7915d4d1ef5203a191d0103baa1a4a18dcfb2b0ee2ef17374fb8114f07391656620c98519378918ac8b31acb8846'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wahrnit::Application.config.session_store :cookie_store, key: '_wahrnit_session'
